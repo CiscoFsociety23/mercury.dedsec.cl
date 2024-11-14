d31011ab1ce823d04c327fe34675fa8950bad058
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-11-14 16:21:10 -0300

    feat: adding view of delivery record

--- src/main/java/com/dedsec/mercury/services/DeliveryRegistryService.java
+++ src/main/java/com/dedsec/mercury/services/DeliveryRegistryService.java
@@ -1,11 +1,14 @@
 package com.dedsec.mercury.services;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.dedsec.mercury.dto.DeliveryRecord;
 import com.dedsec.mercury.interfaces.DeliveryRegistryRepository;
 import com.dedsec.mercury.models.DeliveryRegistry;
 import com.dedsec.mercury.models.Layouts;
@@ -28,6 +31,22 @@ public class DeliveryRegistryService {
         deliveryRegistry.setSendTime(new Date());
         deliveryRegistryRepository.save(deliveryRegistry);
         logger.info("[ METHOD: saveDeliveryRegistry() ]: Registro guardado");
+    }
+
+    public List<DeliveryRecord> getDeliveryRegistry(){
+        logger.info("[ METHOD: getDeliveryRegistry() ]: Obteniendo registro de envios");
+        List<DeliveryRegistry> registry = deliveryRegistryRepository.findAll();
+        List<DeliveryRecord> record = new ArrayList<>();
+        for (DeliveryRegistry deliveryRegistry : registry) {
+            DeliveryRecord deliRecord = new DeliveryRecord();
+            deliRecord.setId(deliveryRegistry.getIdDeliveryRegistry());
+            deliRecord.setReciever(deliveryRegistry.getReciever());
+            deliRecord.setSubject(deliveryRegistry.getSubject());
+            deliRecord.setLayoutName(deliveryRegistry.getLayoutType().getLayoutName());
+            deliRecord.setSendDate(deliveryRegistry.getSendTime());
+            record.add(deliRecord);
+        }
+        return record;
     }
 
 }
